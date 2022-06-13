381974b34aabc2470e676a20cf52d48b50c89335
CrystalKSTW <crystalkstw372000@gmail.com>
2022-06-13 20:27:52 +0630

    Linked Lists Finished

---
+++ linked_lists.rb
@@ -0,0 +1,142 @@
+class LinkedList
+    attr_reader :head, :tail
+  
+    def initialize
+      @head = nil
+      @tail = nil
+    end
+  
+    def append(value)
+      new_node = Node.new value
+      @tail.next_node = new_node if @tail
+      @tail = new_node
+      @head ||= new_node
+    end
+  
+    def prepend(value)
+      new_node = Node.new value
+      new_node.next_node = @head if @head
+      @head = new_node
+      @tail ||= new_node
+    end
+  
+    def size
+      size = 0
+      node = @head
+      until node.nil?
+        size += 1
+        node = node.next_node
+      end
+      size
+    end
+  
+    def at(index)
+      node = @head
+      index.times { node = node.next_node } if index < size
+      node
+    end
+  
+    def pop
+      return if @head.nil?
+  
+      if size == 1
+        @head = nil
+        @tail = nil
+      else
+        @tail = at(size - 2)
+        @tail.next_node = nil
+      end
+    end
+  
+    def contains?(value)
+      node = @head
+      until node.nil?
+        return true if node.value == value
+  
+        node = node.next_node
+      end
+      false
+    end
+  
+    def find(value)
+      node = @head
+      size.times do |idx|
+        return idx if node.value == value
+  
+        node = node.next_node
+      end
+      nil
+    end
+  
+    def to_s
+      node = @head
+      until node.nil?
+        print "( #{node.value} ) -> "
+        node = node.next_node
+      end
+      puts 'nil'
+    end
+  
+    def insert_at(value, index)
+      if index.zero?
+        prepend(value)
+        return
+      end
+      if index == size
+        append(value)
+        return
+      end
+      return unless index < size
+  
+      at_index = at(index)
+      new_node = Node.new value
+      new_node.next_node = at_index
+      before_index = at(index - 1)
+      before_index.next_node = new_node
+    end
+  
+    def remove_at(index)
+      if index == size - 1 || size == 1
+        pop
+        return
+      end
+      if index.zero?
+        @head = @head.next_node
+        return
+      end
+      return unless index < size
+  
+      at_index = at(index)
+      after_index = at_index.next_node
+      before_index = at(index - 1)
+      before_index.next_node = after_index
+    end
+  end
+  
+  class Node
+    attr_accessor :value, :next_node
+  
+    def initialize(value = nil)
+      @value = value
+      @next_node = nil
+    end
+  end
+  
+  # Testing
+  l = LinkedList.new
+  l.append(1)
+  l.append(2)
+  l.prepend(3)
+  l.to_s
+  puts "Size: #{l.size}"
+  puts "Head: #{l.head.value}"
+  puts "Tail: #{l.tail.value}"
+  puts "At index 1: #{l.at(1).value}"
+  l.pop
+  l.to_s
+  puts "Contains 2? #{l.contains?(2)}"
+  puts "Find index of 1: #{l.find(1)}"
+  l.insert_at(5, 1)
+  l.to_s
+  l.remove_at(0)
+  l.to_s
